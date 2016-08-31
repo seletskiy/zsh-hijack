4950482f7b40dc42aae1561ceaae8529ac0e793e
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-08-31 22:41:04 +0700

    get rid of parser, fix highlighting

--- plugin.zsh
+++ plugin.zsh
@@ -1,9 +1,7 @@
 _hijack_skip_history_first=false
-_hijack_command_modified=false
-
 _hijack_transformations=()
 
-function hijack:transform() {
+hijack:transform() {
     zparseopts -a opts -D '-e=is_expression'
 
     local condition=$1
@@ -16,117 +14,79 @@ function hijack:transform() {
     _hijack_transformations+=("$condition" "$transformation")
 }
 
-function hijack:reset() {
+hijack:reset() {
     _hijack_transformations=()
 }
 
-function :hijack:apply() {
-    local command="$1"
+:hijack:apply() {
+    local buffer="$1"
+
     local condition
     local transformation
     local condition_result
-    local new_command
+    local new_buffer
+    local result=1
 
-    local i
-    # typeset -A doesn't work for whatever reason here, however, it works if
-    # applied in ~/.zshrc. WTF!
     for (( i = 1; i < ${#_hijack_transformations}; i += 2 )); do
         condition=${_hijack_transformations[$i]}
         transformation=${_hijack_transformations[$i + 1]}
-        if condition_result=$(eval $condition <<< $command); then
+
+        if condition_result=$(eval $condition <<< $buffer); then
             if [ ! "$transformation" ]; then
-                command=$condition_result
+                if [ "$condition_result" != "$buffer" ]; then
+                    result=0
+                fi
+
+                buffer=$condition_result
             else
-                if new_command=$(eval $transformation <<< $command); then
-                    command=$new_command
+                if new_buffer=$(eval $transformation <<< $buffer); then
+                    result=0
+
+                    buffer=$new_buffer
                 fi
             fi
         fi
     done
 
-    printf "%s" "$command"
+    printf "%s" "$buffer"
+
+    return "$result"
 }
 
-function :hijack:hook() {
+zle -N zle-line-finish :hijack:hook
+
+:hijack:hook() {
     print -S "${BUFFER//\\/\\\\}"
 
-    BUFFER="$(:hijack:foreach-pipe "$BUFFER")"
-}
+    _hijack_skip_history_first=false
 
-function :hijack:foreach-pipe() {
-    local buffer="$1"
-
-    local offset=()
-    local _offsets=()
-    local offsets_var="${2:-_offsets}"
-
-    local parts=(${(z)buffer})
-
-    local curr_pipe_index=1
-    local prev_pipe_index=1
-    local pipe_number=1
-
-    local new_parts=()
-    local command
-    local command_regexp=""
-    local new_command
-
-    local command_part
-    local command_index
-
-    while (( curr_pipe_index <= ${#parts} )); do
-        curr_pipe_index=${parts[(in.pipe_number.)\|]}
-        command=(${parts[prev_pipe_index, curr_pipe_index - 1]})
-        command="${command[@]}"
-
-        new_command=$(:hijack:apply "${command}")
-        if [ "$new_command" != "$command" ]; then
-            _hijack_skip_history_first=true
-            _hijack_command_modified=true
-
-            # FIXME: implement precise highlighting
-            _offsets=(0 ${#command})
+    if BUFFER="$(:hijack:apply "$BUFFER")"; then
+        if type _zsh_highlight >/dev/null; then
+             _zsh_highlight
         fi
 
-        new_parts+=$new_command
-
-        pipe_number+=1
-        prev_pipe_index=$(( curr_pipe_index + 1 ))
-    done
-
-    eval $offsets_var='(${_offsets[@]})'
-
-    printf "%s" "${(j: | :)new_parts}"
-}
-
-function :hijack:drop-history() {
-    return 1
-}
-
-function :hijack:reset-history-skip() {
-    if ! $_hijack_command_modified ; then
-        _hijack_skip_history_first=false
+        _hijack_skip_history_first=true
     fi
-
-    _hijack_command_modified=false
 }
 
-function hijack:history-substring-search-up() {
+zle -N hijack:history-substring-search-up
+
+hijack:history-substring-search-up() {
     zle kill-word
     zle history-substring-search-up
 
     if $_hijack_skip_history_first; then
         zle history-substring-search-up
 
-        :hijack:reset-history-skip
+        _hijack_skip_history_first=false
     fi
 }
 
-zle -N zle-line-finish :hijack:hook
-zle -N hijack:history-substring-search-up
+add-zsh-hook zshaddhistory :hijack:on-history-add
 
-add-zsh-hook zshaddhistory :hijack:drop-history
-add-zsh-hook preexec :hijack:reset-history-skip
+:hijack:on-history-add() {
+    return 1
+}
 
 _zsh_highlight_hijack_highlighter_predicate() {
     _zsh_highlight_buffer_modified
@@ -139,12 +99,9 @@ _zsh_highlight_hijack_highlighter() {
 
     zstyle -g highlighting 'hijack:highlighting'
 
-    :hijack:foreach-pipe "$BUFFER" "offsets" > /dev/null
-
-    local i
-    for (( i = 1; i < ${#offsets}; i += 2 )); do
-        region_highlight+=("${offsets[i]} ${offsets[i + 1]} ${highlighting}")
-    done
+    if :hijack:apply "$BUFFER" > /dev/null; then
+        region_highlight+=("0 ${#BUFFER} ${highlighting}")
+    fi
 }
 
 zstyle 'hijack:highlighting' 'fg=4'
